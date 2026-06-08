39eae4e044c3165fbda339d9721a2ee49a2a6e77
TaurineManiac <ignatladno@gmail.com>
2026-06-08 17:25:23 +0300

    basic project initialization

---
+++ src/main/java/org/example/banksystemfromjavaacademy/BankSystemFromJavaAcademyApplication.java
@@ -0,0 +1,13 @@
+package org.example.banksystemfromjavaacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSystemFromJavaAcademyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankSystemFromJavaAcademyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/banksystemfromjavaacademy/BankSystemFromJavaAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.banksystemfromjavaacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSystemFromJavaAcademyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
