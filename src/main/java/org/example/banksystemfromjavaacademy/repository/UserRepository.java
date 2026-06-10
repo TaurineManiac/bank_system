ca6ff1be8ff4cfa41305a93e676b5ced2fa5794c
TaurineManiac <ignatladno@gmail.com>
2026-06-10 18:21:56 +0300

    Create Repository for User.
    This class stores all basic methods for work with database.

--- src/main/java/org/example/banksystemfromjavaacademy/repository/UserRepository.java
+++ src/main/java/org/example/banksystemfromjavaacademy/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package org.example.banksystemfromjavaacademy.repository;
 
-public interface UserRepository {
+import org.example.banksystemfromjavaacademy.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
 }
