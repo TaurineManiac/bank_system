c7878fcb084b2af30d039ab1059b840d6cbcbd9b
TaurineManiac <ignatladno@gmail.com>
2026-06-13 21:01:33 +0300

    Modified User Entity
    Modified User entity for separate it from Account(new entity). This new entities will have One User - to Many Account relations.

--- src/main/java/org/example/banksystemfromjavaacademy/entity/User.java
+++ src/main/java/org/example/banksystemfromjavaacademy/entity/User.java
@@ -5,8 +5,9 @@ import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Getter
@@ -20,14 +21,14 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true,  nullable = false)
+    private UUID publicUserId;
     private String firstName;
     private String lastName;
     private String otherName;
     private String gender;
     private String address;
     private String stateOfOrigin;
-    private String accountNumber;
-    private BigDecimal accountBalance;
     private String email;
     private String phoneNumber;
     private String alternativePhoneNumber;
@@ -38,4 +39,6 @@ public class User {
     @UpdateTimestamp
     private LocalDateTime modificationDate;
 
+    @OneToMany(mappedBy ="user" , cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,  CascadeType.REFRESH})
+    private List<Account>  accounts;
 }
