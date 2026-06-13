91ab178f11702f4c3d4691bd157fa66dd2184539
TaurineManiac <ignatladno@gmail.com>
2026-06-13 21:10:59 +0300

    Create new request class - AccountRequest.
    This class has only one not blank param - account type(String).

--- src/main/java/org/example/banksystemfromjavaacademy/dto/AccountRequest.java
+++ src/main/java/org/example/banksystemfromjavaacademy/dto/AccountRequest.java
@@ -1,4 +1,9 @@
 package org.example.banksystemfromjavaacademy.dto;
 
+
+import jakarta.validation.constraints.NotBlank;
+
 public class AccountRequest {
+    @NotBlank(message = "Account type is required!")
+    private String accountType;
 }
