d574f2ed443844f67ffa2d4d462e29d1a5f06afe
TaurineManiac <ignatladno@gmail.com>
2026-06-13 18:13:15 +0300

    Create UserServiceImpl
    This is a new service, here will be created methods, that work with User validation.

--- src/main/java/org/example/banksystemfromjavaacademy/service/impl/UserServiceImpl.java
+++ src/main/java/org/example/banksystemfromjavaacademy/service/impl/UserServiceImpl.java
@@ -1,4 +1,67 @@
 package org.example.banksystemfromjavaacademy.service.impl;
 
-public class UserServiceImpl {
+import org.example.banksystemfromjavaacademy.dto.AccountInfo;
+import org.example.banksystemfromjavaacademy.dto.BankResponse;
+import org.example.banksystemfromjavaacademy.dto.UserRequest;
+import org.example.banksystemfromjavaacademy.entity.User;
+import org.example.banksystemfromjavaacademy.repository.UserRepository;
+import org.example.banksystemfromjavaacademy.utils.AccountUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public BankResponse createAccount(UserRequest userRequest) {
+        /**
+         * Creating an account - saving a new user into DB
+         * Next step is check if User already exist.
+         */
+
+        if(userRepository.existsByEmail(userRequest.getEmail())) {
+            return BankResponse.builder()
+                    .responseCode(AccountUtils.ACCOUNT_EXISTS_CODE)
+                    .responseMessage(AccountUtils.ACCOUNT_EXISTS_MESSAGE)
+                    .accountInfo(null)
+                    .build();
+        }
+
+
+
+        User newUser = User.builder()
+                .firstName(userRequest.getFirstName())
+                .lastName(userRequest.getLastName())
+                .otherName(userRequest.getOtherName())
+                .gender(userRequest.getGender())
+                .address(userRequest.getAddress())
+                .stateOfOrigin(userRequest.getStateOfOrigin())
+                .accountNumber(AccountUtils.generateAccountNumber())
+                .accountBalance(BigDecimal.ZERO)
+                .email(userRequest.getEmail())
+                .phoneNumber(userRequest.getPhoneNumber())
+                .alternativePhoneNumber(userRequest.getAlternativePhoneNumber())
+                .status("ACTIVE")
+                .build();
+
+        User savedUser = userRepository.save(newUser);
+
+        return BankResponse.builder()
+                .responseCode(AccountUtils.ACCOUNT_CREATION_SUCCESS)
+                .responseMessage(AccountUtils.ACCOUNT_CREATION_MESSAGE)
+                .accountInfo(AccountInfo.builder()
+                        .accountNumber(savedUser.getAccountNumber())
+                        .accountBalance(String.valueOf(savedUser.getAccountBalance()))
+                        .accountName(savedUser.getFirstName() + " " + savedUser.getLastName() + savedUser.getOtherName())
+                        .build())
+                .build();
+    }
 }
