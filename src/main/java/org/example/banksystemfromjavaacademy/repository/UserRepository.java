5fa8e37053c05b442bcfa7b0845ae276a09a3a74
TaurineManiac <ignatladno@gmail.com>
2026-06-13 18:04:25 +0300

    Update UserRepository
    Was added methods for serach per find User per email/accountId and for check is User exists per email/accountId.

--- src/main/java/org/example/banksystemfromjavaacademy/repository/UserRepository.java
+++ src/main/java/org/example/banksystemfromjavaacademy/repository/UserRepository.java
@@ -2,7 +2,12 @@ package org.example.banksystemfromjavaacademy.repository;
 
 import org.example.banksystemfromjavaacademy.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+    Boolean existsByEmail(String email);
+    Boolean existsByAccountNumber(String account);
+    User findUserByEmail(String email);
+    User findUserByAccountNumber(String account);
 }
