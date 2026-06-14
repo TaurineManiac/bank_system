04c4e8de33137a26ca9f1ca5f5282c2f23ca6c97
TaurineManiac <ignatladno@gmail.com>
2026-06-14 14:24:50 +0300

    Add new param into AccountRequset
    I hardcode it now, later I'll delete it.

--- src/main/java/org/example/banksystemfromjavaacademy/dto/AccountRequest.java
+++ src/main/java/org/example/banksystemfromjavaacademy/dto/AccountRequest.java
@@ -2,8 +2,13 @@ package org.example.banksystemfromjavaacademy.dto;
 
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
 
 public class AccountRequest {
     @NotBlank(message = "Account type is required!")
     private String accountType;
+    @NotNull(message = "Account owner's id is required!")
+    private UUID userPublicId;
 }
