f86057b4fac0ecba0637ee08cdeca4b218486726
TaurineManiac <ignatladno@gmail.com>
2026-06-23 23:05:35 +0300

    Was removed old logic of id generation
    Was removed old logic of id generation. Now it's changed with UUIDv7 generation.

--- src/main/java/org/example/banksystemfromjavaacademy/utils/BankUtils.java
+++ src/main/java/org/example/banksystemfromjavaacademy/utils/BankUtils.java
@@ -1,4 +1,11 @@
 package org.example.banksystemfromjavaacademy.utils;
 
-public interface BankUtils {
+import io.github.robsonkades.uuidv7.UUIDv7;
+
+import java.util.UUID;
+
+public class BankUtils {
+    public static UUID generatePublicId() {
+        return UUIDv7.randomUUID();
+    }
 }
