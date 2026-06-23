f433fe654ef31cbd531cbd3696899345f5173c69
TaurineManiac <ignatladno@gmail.com>
2026-06-23 22:15:54 +0300

    Modified UserInfo class.
    Add implementation of BankInfo interface. It'll be used in BankResponse.

--- src/main/java/org/example/banksystemfromjavaacademy/dto/UserInfo.java
+++ src/main/java/org/example/banksystemfromjavaacademy/dto/UserInfo.java
@@ -1,4 +1,39 @@
 package org.example.banksystemfromjavaacademy.dto;
 
-public class UserInfo {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class UserInfo implements BankInfo{
+    @NotEmpty
+    private UUID publicUserId;
+    @NotBlank
+    private String firstName;
+    @NotBlank
+    private String lastName;
+    @NotBlank
+    private String otherName;
+    @NotBlank
+    private String gender;
+    @NotBlank
+    private String address;
+    @NotBlank
+    private String stateOfOrigin;
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String phoneNumber;
+    @NotBlank
+    private String alternativePhoneNumber;
+    @NotBlank
+    private String status;
 }
