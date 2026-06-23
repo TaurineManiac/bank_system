0ca475b2655da707116e4b692427ee298e7b3961
TaurineManiac <ignatladno@gmail.com>
2026-06-23 22:44:34 +0300

    Was created UserResponseConstants enum
    This is a enum with params for user response messages.

--- src/main/java/org/example/banksystemfromjavaacademy/enums/UserResponseConstants.java
+++ src/main/java/org/example/banksystemfromjavaacademy/enums/UserResponseConstants.java
@@ -1,4 +1,14 @@
 package org.example.banksystemfromjavaacademy.enums;
 
-public class UserResponseConstants {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum UserResponseConstants {
+    USER_ALREADY_EXISTS("003", "This User already exists!"),
+    USER_CREATION_SUCCESS("004", "User successfully created!");
+
+    private final String responseCode;
+    private final String responseMessage;
 }
