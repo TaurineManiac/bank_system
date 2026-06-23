4a0c9346e0ff774ceceae9c08c9a770597c6a402
TaurineManiac <ignatladno@gmail.com>
2026-06-23 22:20:44 +0300

    Modified UserRepository
    There was deleted 2 methods, that was never used/overrided. And fix Generics in Jpa implementation for a bit.

--- src/main/java/org/example/banksystemfromjavaacademy/repository/UserRepository.java
+++ src/main/java/org/example/banksystemfromjavaacademy/repository/UserRepository.java
@@ -5,9 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByEmail(String email);
-    Boolean existsByAccountNumber(String account);
     User findUserByEmail(String email);
-    User findUserByAccountNumber(String account);
 }
