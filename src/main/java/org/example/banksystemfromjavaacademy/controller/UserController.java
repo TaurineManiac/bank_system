4c720318ed6689607e63a999d68c47448c7653c1
TaurineManiac <ignatladno@gmail.com>
2026-06-24 11:12:14 +0300

    Create UserController class
    Common controller for first beta version. I'll add an exceptions, that will be called from service, if smth wrong happen.

--- src/main/java/org/example/banksystemfromjavaacademy/controller/UserController.java
+++ src/main/java/org/example/banksystemfromjavaacademy/controller/UserController.java
@@ -1,28 +1,29 @@
 package org.example.banksystemfromjavaacademy.controller;
 
-import org.example.banksystemfromjavaacademy.dto.AccountRequest;
 import org.example.banksystemfromjavaacademy.dto.BankResponse;
-import org.example.banksystemfromjavaacademy.entity.Account;
+import org.example.banksystemfromjavaacademy.dto.UserRequest;
 import org.example.banksystemfromjavaacademy.service.impl.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/account")
-public class AccountController {
+@RequestMapping("/api/user")
+public class UserController {
 
-    private final AccuntService userService;
+    private final UserService userService;
 
-    public AccountController(UserService userService) {
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @PostMapping("/create_account")
-    public Account createAccount(@RequestBody AccountRequest account){
-        BankResponse bankResponse = new BankResponse();
-        return
+    @PostMapping("/create_user")
+    public ResponseEntity createAccount(@RequestBody UserRequest userRequest){
+        BankResponse response= userService.createUser(userRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
 }
