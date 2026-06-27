1d3a221d6aa3ca3d1ca7abf9d6541693ecba97df
TaurineManiac <ignatladno@gmail.com>
2026-06-27 15:46:01 +0300

    Modified AccountResponseConstants and UserResponseConstants enums
    Was changed naming and added new param - http status.

--- src/main/java/org/example/banksystemfromjavaacademy/enums/AccountResponseConstants.java
+++ src/main/java/org/example/banksystemfromjavaacademy/enums/AccountResponseConstants.java
@@ -2,14 +2,17 @@ package org.example.banksystemfromjavaacademy.enums;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 @Getter
-public enum AccountResponseConstants {
-    ACCOUNT_ALREADY_EXISTS("001", "This account already exists"),
-    ACCOUNT_CREATION_SUCCESS("002", "Account successfully created");
+public enum AccountResponseConstants implements ResponseCode {
+    ACCOUNT_ALREADY_EXISTS("001", "This account already exists", HttpStatus.CONFLICT),
+    ACCOUNT_CREATION_SUCCESS("002", "Account successfully created",  HttpStatus.CREATED),
+    ACCOUNT_NOT_FOUND("005", "Account not found",  HttpStatus.NOT_FOUND);
 
 
-    private final String responseCode;
-    private final String responseMessage;
+    private final String code;
+    private final String message;
+    private final HttpStatus httpStatus;
 }
--- src/main/java/org/example/banksystemfromjavaacademy/enums/UserResponseConstants.java
+++ src/main/java/org/example/banksystemfromjavaacademy/enums/UserResponseConstants.java
@@ -2,13 +2,16 @@ package org.example.banksystemfromjavaacademy.enums;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 @Getter
-public enum UserResponseConstants {
-    USER_ALREADY_EXISTS("003", "This User already exists!"),
-    USER_CREATION_SUCCESS("004", "User successfully created!");
+public enum UserResponseConstants implements ResponseCode {
+    USER_ALREADY_EXISTS("003", "User already exists", HttpStatus.CONFLICT),
+    USER_CREATION_SUCCESS("004", "User successfully created", HttpStatus.CREATED),
+    USER_NOT_FOUND("006", "User not found", HttpStatus.NOT_FOUND);
 
-    private final String responseCode;
-    private final String responseMessage;
+    private final String code;
+    private final String message;
+    private final HttpStatus httpStatus;
 }
