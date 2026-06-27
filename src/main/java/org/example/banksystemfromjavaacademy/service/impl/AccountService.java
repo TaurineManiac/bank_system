9e177f83c5e70c900a1410e9b9bfbd35ca75729b
TaurineManiac <ignatladno@gmail.com>
2026-06-27 16:00:26 +0300

    Created AccountService interface
    Basic interface with one create method.

--- src/main/java/org/example/banksystemfromjavaacademy/service/impl/AccountService.java
+++ src/main/java/org/example/banksystemfromjavaacademy/service/impl/AccountService.java
@@ -1,4 +1,8 @@
 package org.example.banksystemfromjavaacademy.service.impl;
 
+import org.example.banksystemfromjavaacademy.dto.AccountRequest;
+import org.example.banksystemfromjavaacademy.dto.BankResponse;
+
 public interface AccountService {
+    BankResponse createAccount(AccountRequest accountRequest);
 }
