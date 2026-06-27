3a802d7b233d8ff9ab1ebc5db76ff7a1f7e5464d
TaurineManiac <ignatladno@gmail.com>
2026-06-27 17:50:07 +0300

    Modified GlobalExceptionHandler class
    Was added handler to new UserAlreadyExistsException.java exception.

--- src/main/java/org/example/banksystemfromjavaacademy/handler/GlobalExceptionHandler.java
+++ src/main/java/org/example/banksystemfromjavaacademy/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package org.example.banksystemfromjavaacademy.handler;
 
 import org.example.banksystemfromjavaacademy.dto.BankResponse;
+import org.example.banksystemfromjavaacademy.exceptions.UserAlreadyExistsException;
 import org.example.banksystemfromjavaacademy.exceptions.UserNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,6 +18,16 @@ public class GlobalExceptionHandler {
                 .data(null)
                 .build();
 
+        return ResponseEntity.status(ex.getHttpStatus()).body(response);
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public  ResponseEntity<BankResponse<?>> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
+        BankResponse<?> response = BankResponse.builder()
+                .responseCode(ex.getCode())
+                .responseMessage(ex.getMessage())
+                .data(null)
+                .build();
         return ResponseEntity.status(ex.getHttpStatus()).body(response);
     }
 }
