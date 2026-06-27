da2a65b55c1e9d5cddd8952d236b60870e1362cd
TaurineManiac <ignatladno@gmail.com>
2026-06-27 17:21:54 +0300

    Created AccountController class
    Common controller with create endpoint

--- src/main/java/org/example/banksystemfromjavaacademy/controller/AccountController.java
+++ src/main/java/org/example/banksystemfromjavaacademy/controller/AccountController.java
@@ -1,4 +1,26 @@
 package org.example.banksystemfromjavaacademy.controller;
 
+
+import org.example.banksystemfromjavaacademy.dto.AccountRequest;
+import org.example.banksystemfromjavaacademy.dto.BankResponse;
+import org.example.banksystemfromjavaacademy.service.impl.AccountService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/account")
 public class AccountController {
+
+    AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping("create_account")
+    public ResponseEntity<BankResponse> getAccount(@RequestBody AccountRequest accountRequest) {
+        BankResponse response= accountService.createAccount(accountRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
