21372759597c41314f7c195766a741babdc589c7
TaurineManiac <ignatladno@gmail.com>
2026-06-27 15:50:11 +0300

    Created UserNotFoundException exception
    It creating for return to user, that smth wrong happen(here is program didn't find relative user).

--- src/main/java/org/example/banksystemfromjavaacademy/exceptions/UserNotFoundException.java
+++ src/main/java/org/example/banksystemfromjavaacademy/exceptions/UserNotFoundException.java
@@ -1,4 +1,19 @@
 package org.example.banksystemfromjavaacademy.exceptions;
 
-public class UserNotFoundException {
+import lombok.Getter;
+import org.example.banksystemfromjavaacademy.enums.ResponseCode;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class UserNotFoundException extends RuntimeException {
+    private final String code;
+    private final HttpStatus httpStatus;
+
+
+
+    public UserNotFoundException(ResponseCode responseCode) {
+        super(responseCode.getMessage());
+        this.code = responseCode.getCode();
+        this.httpStatus = responseCode.getHttpStatus();
+    }
 }
