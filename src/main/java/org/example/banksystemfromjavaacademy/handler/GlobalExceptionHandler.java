0e1465ee9ba0a1545d36112abf1f8cdadf69288f
TaurineManiac <ignatladno@gmail.com>
2026-06-27 15:52:20 +0300

    Created GlobalExceptionHandler class
    It is common exception handler.

--- src/main/java/org/example/banksystemfromjavaacademy/handler/GlobalExceptionHandler.java
+++ src/main/java/org/example/banksystemfromjavaacademy/handler/GlobalExceptionHandler.java
@@ -1,4 +1,22 @@
 package org.example.banksystemfromjavaacademy.handler;
 
+import org.example.banksystemfromjavaacademy.dto.BankResponse;
+import org.example.banksystemfromjavaacademy.exceptions.UserNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<BankResponse<?>> handleUserNotFoundException(UserNotFoundException ex) {
+        BankResponse<?> response = BankResponse.builder()
+                .responseCode(ex.getCode())
+                .responseMessage(ex.getMessage())
+                .data(null)
+                .build();
+
+        return ResponseEntity.status(ex.getHttpStatus()).body(response);
+    }
 }
